d88cbcd40c100b4c18d1c59a59f50e6f9d2b98db
Kevin Choye <kevinchoye@Kevins-MacBook-Pro.local>
2022-09-28 14:19:31 -0400

    First Commit

--- discussion_5.py
+++ discussion_5.py
@@ -1,16 +1,19 @@
+from itertools import count
+from tkinter import X
 import unittest
 
 # Counts the number of a's in a sentence (e.g., a string)
 def count_a(sentence):
-	total = 0
-	for i in range(len(sentence) - 1):
-		if i == 'a':
-			total += 1
-	return total
+	count = 0
+	for i in sentence:
+		if i=='A' or i== 'a':
+			count+= 1
+		
+	return count
 
 
-# Item class
-# Describes an item to be sold. Each item has a name, a price, and a stock.
+# # Item class
+# # Describes an item to be sold. Each item has a name, a price, and a stock.
 class Item:
 	# Constructor.
 	def __init__(self, name, price, stock):
@@ -38,15 +41,30 @@ class Warehouse:
 
 	# Adds an item to the warehouse	
 	def add_item(self, item):
-		pass
+		if item not in self.items:
+			self.items.append(item)
+			
 
-	# Returns the item in the warehouse with the most stock		
-	def get_max_stock(self):
 		pass
+# Returns the item in the warehouse with the most stock		
+	def get_max_stock(self):
+		max = self.items[0].stock
+		max_item = self.items[0]
+		for item in self.items:
+			if item.stock > max:
+				max = item.stock
+				max_item = item
+		return max_item
 	
 	# Returns the item in the warehouse with the highest price
 	def get_max_price(self):
-		pass	
+		max = self.items[0].price
+		max_item = self.items[0]
+		for item in self.items:
+			if item.price > max:
+				max = item.price
+				max_item = item
+		return max_item
 
 
 
@@ -60,26 +78,33 @@ class TestAllMethods(unittest.TestCase):
 		self.item3 = Item("Water", 1, 100)
 		self.item4 = Item("Fanta", 2, 60)
 		self.item5 = Item("CocaCola", 3, 40)
+		self.warehouse1 = Warehouse([self.item1, self.item2, self.item3, self.item4])
+		self.warehouse2 = Warehouse([self.item1, self.item2, self.item3, self.item4, self.item5])
 
-	## Check to see whether count_a works
+
+	# Check to see whether count_a works
 	def test_count_a(self):
+		self.assertEqual(count_a("Apple"),1)
+		self.assertEqual(count_a("Class"),1)
 		pass
 
 
 	## Check to see whether you can add an item to the warehouse
 	def test_add_item(self):
-		pass
+		foodlist=Warehouse(items=[self.item1,self.item2])
+		foodlist.add_item(self.item3)	
+		self.assertEqual(foodlist.items,[self.item1,self.item2,self.item3])
 
 
 	## Check to see whether warehouse correctly returns the item with the most stock
 	def test_warehouse_max_stocks(self):
-		pass
+		self.assertEqual(self.warehouse1.get_max_stock(), self.item3)
 
 
 	# Check to see whether the warehouse correctly return the item with the highest price
 	def test_warehouse_max_price(self):
-		pass
-		
+		self.assertEqual(self.warehouse1.get_max_price(), self.item1)		
+
 
 def main():
 	unittest.main()
